81d2ff1805f97dbc827e55b7c4397bed4a66d4d9
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-04-12 15:28:21 +0800

    Add input todo focus directive

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,10 @@
     
     <div v-for="(todo, index) in todos" :key="todo.id">
         <div class="todo-item">
-            {{ todo.title }}
+            <div class="todo-item-left">
+                <div v-if="!todo.isEditing" @dblclick="editTodo(todo)" class="todo-item-label">{{ todo.title }}</div>
+                <input v-else class="todo-item-edit" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" type="text" v-model="todo.title" v-focus>
+            </div>
             <div class="remove-item" @click="removeTodo(index)">
                 &times;
             </div>
@@ -27,11 +30,13 @@ export default {
                   'id': 1,
                   'title': 'Finish Vue Screencast',
                   'completed': false,
+                  'isEditing': false,
               },
               {
                   'id': 2,
                   'title': 'Take over world',
-                  'completed': false
+                  'completed': false,
+                  'isEditing': false
               },
           ]
       }
@@ -48,7 +53,8 @@ export default {
           this.todos.push({
               id: this.idForTodo,
               title: this.newTodo,
-              completed: false
+              completed: false,
+              isEditing: false
           });
 
           this.idForTodo++;
@@ -56,8 +62,22 @@ export default {
       },
       removeTodo(index){
           this.todos.splice(index, 1);
+      },
+      editTodo(todo){
+          todo.isEditing = true;
+      },
+      doneEdit(todo){
+          todo.isEditing = false;
       }
-  }
+  },
+  directives: {
+        focus: {
+            // directive definition
+            inserted: function (el) {
+                el.focus()
+            }
+        }
+    }
 }
 </script>
 
@@ -87,4 +107,30 @@ export default {
             color: black;
         }
     }
+
+    .todo-item-left{
+        display: flex;
+        align-items: center;
+    }
+
+    .todo-item-label{
+        padding: 10px;
+        border: 1px solid white;
+        margin-left: 12px;
+    }
+
+    .todo-item-edit{
+        font-size: 24px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+        &:focus{
+            outline: none;
+        }
+    }
+
 </style>
